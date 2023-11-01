b6ecff004db70e0b6fe032a2d0b81f2fdb39f2ee
Micah Strange <micasans@btinternet.com>
2023-11-01 14:08:36 +0000

    support later versions of JUCE (temporary)

--- melatonin/parameter_test_helpers.h
+++ melatonin/parameter_test_helpers.h
@@ -2,19 +2,20 @@
 
 namespace melatonin
 {
-    // You'll want to use this anytime you call someParam->setValueNotifyingHost (1.0f);
-    static inline void waitForParameterChange()
-    {
-        juce::MessageManager::getInstance()->runDispatchLoopUntil (1);
-        juce::Timer::callPendingTimersSynchronously();
-    }
+// You'll want to use this anytime you call someParam->setValueNotifyingHost (1.0f);
+static inline void waitForParameterChange ()
+{
+    // Since we're currently not using parameter helpers this has been removed to support later
+    // versions of JUCE juce::MessageManager::getInstance ()->runDispatchLoopUntil (1);
+    juce::Timer::callPendingTimersSynchronously ();
+}
 
-    // the apvts can take up to half a second (!) due to back off to flush its state
-    // the only way to circumvent and get flushParameterValuesToValueTree to fire is by copying state
-    // so we (pointlessly) copy the state and then run the dispatch loop
-    static inline void flushAPVTS (juce::AudioProcessorValueTreeState& apvts)
-    {
-        auto unusedState = apvts.copyState();
-        waitForParameterChange();
-    }
+// the apvts can take up to half a second (!) due to back off to flush its state
+// the only way to circumvent and get flushParameterValuesToValueTree to fire is by copying state
+// so we (pointlessly) copy the state and then run the dispatch loop
+static inline void flushAPVTS (juce::AudioProcessorValueTreeState & apvts)
+{
+    auto unusedState = apvts.copyState ();
+    waitForParameterChange ();
+}
 }
